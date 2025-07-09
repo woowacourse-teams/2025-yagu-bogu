e951817d40bca974481ae6cd58110ef2aed5fbf5
Mint <audwl03071@gmail.com>
2025-07-09 19:12:40 +0900

    chore: project initial setup

---
+++ backend/src/test/java/com/aniwhere/AniwhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniwhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniwhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
