dcd11384fe5f89e573e0b39ca0ad3c5ebb84120d
JunHyuk Im <imjh0417@gmail.com>
2025-07-19 15:52:58 +0900

    feat: 통계 TabLayout 구현

--- android/app/src/main/java/com/yagubogu/presentation/stats/StatsFragment.kt
+++ android/app/src/main/java/com/yagubogu/presentation/stats/StatsFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.google.android.material.tabs.TabLayout
 import com.yagubogu.databinding.FragmentStatsBinding
+import com.yagubogu.presentation.stats.my.MyStatsFragment
+import com.yagubogu.presentation.stats.stadium.StadiumListFragment
 
 @Suppress("ktlint:standard:backing-property-naming")
 class StatsFragment : Fragment() {
@@ -18,11 +22,38 @@ class StatsFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentStatsBinding.inflate(inflater, container, false)
+        setupTabLayoutListener()
+        replaceFragment(MyStatsFragment::class.java)
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setupTabLayoutListener() {
+        binding.tabStats.addOnTabSelectedListener(
+            object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    when (tab?.position) {
+                        0 -> replaceFragment(MyStatsFragment::class.java)
+                        1 -> replaceFragment(StadiumListFragment::class.java)
+                    }
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) = Unit
+
+                override fun onTabReselected(tab: TabLayout.Tab?) = Unit
+            },
+        )
+    }
+
+    private fun replaceFragment(fragment: Class<out Fragment>): Boolean {
+        childFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(binding.fcvStatsFragment.id, fragment, null)
+        }
+        return true
     }
 }
