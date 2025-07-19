85e3ed98643b9d970c1615ad9ab4dc3223aa1a95
jiyunlee <02leejiyun@naver.com>
2025-07-19 19:25:35 +0900

    feat: 현재 위치 정보 가져오는 LocationProvider 구현

---
+++ android/app/src/main/java/com/yagubogu/presentation/home/LocationProvider.kt
@@ -0,0 +1,44 @@
+package com.yagubogu.presentation.home
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+
+class LocationProvider(
+    private val context: Context,
+) {
+    private val locationClient = LocationServices.getFusedLocationProviderClient(context)
+
+    @SuppressLint("MissingPermission")
+    fun fetchCurrentLocation(
+        onSuccess: (Location) -> Unit,
+        onFailure: (Exception) -> Unit,
+    ) {
+        locationClient
+            .getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+            .addOnSuccessListener { location: Location ->
+                onSuccess(location)
+            }.addOnFailureListener { exception: Exception ->
+                onFailure(exception)
+            }
+    }
+
+    fun isLocationPermissionGranted(): Boolean = isFineLocationPermissionGranted() || isCoarseLocationPermissionGranted()
+
+    private fun isFineLocationPermissionGranted(): Boolean =
+        ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun isCoarseLocationPermissionGranted(): Boolean =
+        ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        ) == PackageManager.PERMISSION_GRANTED
+}
