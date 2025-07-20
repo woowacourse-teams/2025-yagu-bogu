9e103faa521761b6ec7737b34edef0fe2930bab9
jiyunlee <02leejiyun@naver.com>
2025-07-20 14:40:54 +0900

    feat: Fragment 전환 시 Toolbar 제목 변경

--- android/app/src/main/java/com/yagubogu/presentation/record/RecordFragment.kt
+++ android/app/src/main/java/com/yagubogu/presentation/record/RecordFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.yagubogu.MainActivity
+import com.yagubogu.R
 import com.yagubogu.databinding.FragmentRecordBinding
 
 @Suppress("ktlint:standard:backing-property-naming")
@@ -19,6 +21,11 @@ class RecordFragment : Fragment() {
     ): View {
         _binding = FragmentRecordBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (requireActivity() as MainActivity).setToolbarTitle(getString(R.string.bottom_navigation_record))
     }
 
     override fun onDestroyView() {
