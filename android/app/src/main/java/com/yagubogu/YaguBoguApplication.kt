f185712f5bca498ddc43bb4327fe3dcbd1d6350b
jiyunlee <02leejiyun@naver.com>
2025-07-22 13:38:42 +0900

    feat: 위치 정보 관련 의존성 주입 설정

--- android/app/src/main/java/com/yagubogu/YaguBoguApplication.kt
+++ android/app/src/main/java/com/yagubogu/YaguBoguApplication.kt
@@ -1,5 +1,13 @@
 package com.yagubogu
 
 import android.app.Application
+import com.google.android.gms.location.LocationServices
+import com.yagubogu.data.datasource.LocationLocalDataSource
+import com.yagubogu.data.repository.LocationDefaultRepository
 
-class YaguBoguApplication : Application()
+class YaguBoguApplication : Application() {
+    private val locationClient by lazy { LocationServices.getFusedLocationProviderClient(this) }
+
+    private val locationLocalDataSource by lazy { LocationLocalDataSource(locationClient) }
+    val locationRepository by lazy { LocationDefaultRepository(locationLocalDataSource) }
+}
