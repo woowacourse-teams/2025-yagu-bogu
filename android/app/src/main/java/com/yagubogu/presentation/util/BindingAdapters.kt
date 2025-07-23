832b549ad53fa262aeca2c402eae47628540d0db
medAndro <med4ndro@gmail.com>
2025-07-23 20:41:50 +0900

    refactor: 잘못된 backgroundTint null 처리 삭제

--- android/app/src/main/java/com/yagubogu/presentation/util/BindingAdapters.kt
+++ android/app/src/main/java/com/yagubogu/presentation/util/BindingAdapters.kt
@@ -2,14 +2,15 @@ package com.yagubogu.presentation.util
 
 import android.content.res.ColorStateList
 import android.view.View
-import androidx.databinding.BindingAdapter
 import androidx.core.view.ViewCompat
+import androidx.databinding.BindingAdapter
 
 @BindingAdapter("app:backgroundTint")
-fun setBackgroundTint(view: View, colorRes: Int?) {
+fun setBackgroundTint(
+    view: View,
+    colorRes: Int?,
+) {
     if (colorRes != null) {
         ViewCompat.setBackgroundTintList(view, ColorStateList.valueOf(view.context.getColor(colorRes)))
-    } else {
-        ViewCompat.setBackgroundTintList(view, null)
     }
 }
