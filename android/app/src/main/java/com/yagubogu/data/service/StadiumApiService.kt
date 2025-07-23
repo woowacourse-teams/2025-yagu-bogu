7ff757b40497c91c0ad13138e78f0ad638e3ac62
jiyunlee <02leejiyun@naver.com>
2025-07-24 01:08:55 +0900

    feat: Stadium API 관련 파일 추가

---
+++ android/app/src/main/java/com/yagubogu/data/service/StadiumApiService.kt
@@ -0,0 +1,9 @@
+package com.yagubogu.data.service
+
+import com.yagubogu.data.dto.response.StadiumsResponse
+import retrofit2.http.GET
+
+interface StadiumApiService {
+    @GET("/api/stadiums")
+    suspend fun getStadiums(): StadiumsResponse
+}
--- android/app/src/main/java/com/yagubogu/domain/model/Stadium.kt
+++ android/app/src/main/java/com/yagubogu/domain/model/Stadium.kt
@@ -1,15 +1,9 @@
 package com.yagubogu.domain.model
 
-enum class Stadium(
+data class Stadium(
+    val id: Long,
+    val fullName: String,
+    val shortName: String,
+    val location: String,
     val coordinate: Coordinate,
-) {
-    JAMSIL(Coordinate(Latitude(37.512192), Longitude(127.072055))),
-    GOCHUK(Coordinate(Latitude(37.498191), Longitude(126.867073))),
-    INCHEON(Coordinate(Latitude(37.437196), Longitude(126.693294))),
-    SUWON(Coordinate(Latitude(37.299977), Longitude(127.009690))),
-    DAEJEON(Coordinate(Latitude(36.316589), Longitude(127.431211))),
-    GWANGJU(Coordinate(Latitude(35.168282), Longitude(126.889138))),
-    DAEGU(Coordinate(Latitude(35.841318), Longitude(128.681559))),
-    BUSAN(Coordinate(Latitude(35.194146), Longitude(129.061497))),
-    CHANGWON(Coordinate(Latitude(35.222754), Longitude(128.582251))),
-}
+)
