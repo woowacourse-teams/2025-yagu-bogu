14b06d6e41a48cd1eac457b39b51504cab7e8acd
jiyunlee <02leejiyun@naver.com>
2025-07-24 01:13:47 +0900

    feat: Application에 Repository 의존성 추가

--- android/app/src/main/java/com/yagubogu/YaguBoguApplication.kt
+++ android/app/src/main/java/com/yagubogu/YaguBoguApplication.kt
@@ -2,12 +2,23 @@ package com.yagubogu
 
 import android.app.Application
 import com.google.android.gms.location.LocationServices
+import com.yagubogu.data.datasource.CheckInsRemoteDataSource
 import com.yagubogu.data.datasource.LocationLocalDataSource
+import com.yagubogu.data.datasource.StadiumRemoteDataSource
+import com.yagubogu.data.network.RetrofitInstance
+import com.yagubogu.data.repository.CheckInsDefaultRepository
 import com.yagubogu.data.repository.LocationDefaultRepository
+import com.yagubogu.data.repository.StadiumDefaultRepository
 
 class YaguBoguApplication : Application() {
     private val locationClient by lazy { LocationServices.getFusedLocationProviderClient(this) }
 
     private val locationLocalDataSource by lazy { LocationLocalDataSource(locationClient) }
     val locationRepository by lazy { LocationDefaultRepository(locationLocalDataSource) }
+
+    private val stadiumRemoteDataSource by lazy { StadiumRemoteDataSource(RetrofitInstance.stadiumApiService) }
+    val stadiumRepository by lazy { StadiumDefaultRepository(stadiumRemoteDataSource) }
+
+    private val checkInsRemoteDataSource by lazy { CheckInsRemoteDataSource(RetrofitInstance.checkInsApiService) }
+    val checkInsRepository by lazy { CheckInsDefaultRepository(checkInsRemoteDataSource) }
 }
