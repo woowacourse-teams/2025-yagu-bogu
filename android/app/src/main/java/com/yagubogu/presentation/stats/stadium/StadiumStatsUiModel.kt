1b3bce5929cddfbb7d0775928165a969cc10c228
medAndro <med4ndro@gmail.com>
2025-07-23 21:08:02 +0900

    refactor: 오늘의 구장 통계 UI 모델의 기본값 지정

--- android/app/src/main/java/com/yagubogu/presentation/stats/stadium/StadiumStatsUiModel.kt
+++ android/app/src/main/java/com/yagubogu/presentation/stats/stadium/StadiumStatsUiModel.kt
@@ -1,5 +1,6 @@
 package com.yagubogu.presentation.stats.stadium
 
+import com.yagubogu.R
 import java.time.LocalTime
 
 data class StadiumStatsUiModel(
@@ -7,13 +8,17 @@ data class StadiumStatsUiModel(
     val teams: List<TeamStatus>,
 ) {
     val refreshTime: LocalTime get() = LocalTime.now()
-    val teamSize: Int get() = teams.size
+    private val teamSize: Int get() = teams.size
 
-    val firstTeam: TeamStatus? get() = teams.getOrNull(0)
-    val secondTeam: TeamStatus? get() = teams.getOrNull(1)
-    val thirdTeam: TeamStatus? get() = teams.getOrNull(2)
+    val firstTeam: TeamStatus get() = teams.getOrElse(0) { DEFAULT_TEAM_STATUS }
+    val secondTeam: TeamStatus get() = teams.getOrElse(1) { DEFAULT_TEAM_STATUS }
+    val thirdTeam: TeamStatus get() = teams.getOrElse(2) { DEFAULT_TEAM_STATUS }
 
     val showFirstLegend: Boolean get() = teamSize >= 1
     val showSecondLegend: Boolean get() = teamSize >= 2
     val showThirdLegend: Boolean get() = teamSize >= 3
+
+    companion object {
+        private val DEFAULT_TEAM_STATUS = TeamStatus("", R.color.white, 0)
+    }
 }
