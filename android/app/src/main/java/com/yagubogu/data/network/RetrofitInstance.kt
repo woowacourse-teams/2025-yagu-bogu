71a352595579214efbbae6bfc634c1848e90d803
jiyunlee <02leejiyun@naver.com>
2025-07-23 17:11:47 +0900

    feat: HttpLoggingInterceptor 설정

--- android/app/src/main/java/com/yagubogu/data/network/RetrofitInstance.kt
+++ android/app/src/main/java/com/yagubogu/data/network/RetrofitInstance.kt
@@ -3,14 +3,33 @@ package com.yagubogu.data.network
 import com.yagubogu.BuildConfig
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 object RetrofitInstance {
-    private val retrofit by lazy {
+    private val httpLoggingInterceptor: HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level =
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else {
+                    HttpLoggingInterceptor.Level.NONE
+                }
+        }
+
+    private val interceptorClient: OkHttpClient =
+        OkHttpClient()
+            .newBuilder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    private val retrofit: Retrofit by lazy {
         Retrofit
             .Builder()
             .baseUrl(BuildConfig.BASE_URL)
+            .client(interceptorClient)
             .addConverterFactory(
                 Json.asConverterFactory("application/json; charset=UTF8".toMediaType()),
             ).build()
