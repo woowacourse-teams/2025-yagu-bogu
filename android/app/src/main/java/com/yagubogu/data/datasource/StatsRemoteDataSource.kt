a7a81e64dd753079881119ba2bd7d73b59776121
JunHyuk Im <imjh0417@gmail.com>
2025-07-24 15:24:03 +0900

    feat: 통계 Repository, DataSource 구현

---
+++ android/app/src/main/java/com/yagubogu/data/datasource/StatsRemoteDataSource.kt
@@ -0,0 +1,35 @@
+package com.yagubogu.data.datasource
+
+import com.yagubogu.data.dto.response.StatsCountsResponse
+import com.yagubogu.data.dto.response.StatsLuckyStadiumsResponse
+import com.yagubogu.data.dto.response.StatsWinRateResponse
+import com.yagubogu.data.service.StatsApiService
+import com.yagubogu.data.util.safeApiCall
+
+class StatsRemoteDataSource(
+    private val statsApiService: StatsApiService,
+) : StatsDataSource {
+    override suspend fun getStatsWinRate(
+        memberId: Long,
+        year: Int,
+    ): Result<StatsWinRateResponse> =
+        safeApiCall {
+            statsApiService.getStatsWinRate(memberId, year)
+        }
+
+    override suspend fun getStatsCounts(
+        memberId: Long,
+        year: Int,
+    ): Result<StatsCountsResponse> =
+        safeApiCall {
+            statsApiService.getStatsCounts(memberId, year)
+        }
+
+    override suspend fun getLuckyStadiums(
+        memberId: Long,
+        year: Int,
+    ): Result<StatsLuckyStadiumsResponse> =
+        safeApiCall {
+            statsApiService.getLuckyStadiums(memberId, year)
+        }
+}
