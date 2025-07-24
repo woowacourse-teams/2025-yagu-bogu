c47e6f3bbc93e90a4417aec4aa35af977806da5d
jiyunlee <02leejiyun@naver.com>
2025-07-24 22:30:38 +0900

    feat: 응원 팀 조회 API에 캐싱 적용

--- android/app/src/main/java/com/yagubogu/data/repository/MemberDefaultRepository.kt
+++ android/app/src/main/java/com/yagubogu/data/repository/MemberDefaultRepository.kt
@@ -7,10 +7,23 @@ import com.yagubogu.domain.repository.MemberRepository
 class MemberDefaultRepository(
     private val memberDataSource: MemberDataSource,
 ) : MemberRepository {
-    override suspend fun getFavoriteTeam(memberId: Long): Result<String> =
-        memberDataSource
+    private var cachedFavoriteTeam: String? = null
+
+    override suspend fun getFavoriteTeam(memberId: Long): Result<String> {
+        cachedFavoriteTeam?.let { favoriteTeam: String ->
+            return Result.success(favoriteTeam)
+        }
+        return memberDataSource
             .getFavoriteTeam(memberId)
             .map { memberFavoriteResponse: MemberFavoriteResponse ->
-                memberFavoriteResponse.favorite
+                val favoriteTeam = memberFavoriteResponse.favorite
+                cachedFavoriteTeam = favoriteTeam
+                favoriteTeam
             }
+    }
+
+    // TODO: 응원팀을 수정한다면 캐시된 데이터를 invalidate 해야 됨
+    fun invalidateCache() {
+        cachedFavoriteTeam = null
+    }
 }
