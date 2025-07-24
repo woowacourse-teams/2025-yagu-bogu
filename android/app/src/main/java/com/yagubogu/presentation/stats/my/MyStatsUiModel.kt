3d39ad8e06e9bc653f2d8a3c40a0f3bed55e4969
JunHyuk Im <imjh0417@gmail.com>
2025-07-24 15:27:17 +0900

    refactor: 내 통계 UiModel 프로퍼티 추가

--- android/app/src/main/java/com/yagubogu/presentation/stats/my/MyStatsUiModel.kt
+++ android/app/src/main/java/com/yagubogu/presentation/stats/my/MyStatsUiModel.kt
@@ -1,13 +1,16 @@
 package com.yagubogu.presentation.stats.my
 
-import kotlin.math.roundToInt
-
 data class MyStatsUiModel(
     val winCount: Int,
     val drawCount: Int,
     val loseCount: Int,
+    val totalCount: Int,
+    val winningPercentage: Int,
+    val luckyStadium: String,
 ) {
-    val totalCount: Int get() = winCount + drawCount + loseCount
-    val winningPercentage: Int get() = ((winCount.toFloat() / totalCount) * 100).roundToInt()
-    val etcPercentage: Int get() = 100 - winningPercentage
+    val etcPercentage: Int get() = FULL_PERCENTAGE - winningPercentage
+
+    companion object {
+        private const val FULL_PERCENTAGE = 100
+    }
 }
