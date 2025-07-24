9e12481053142a0aef09192bbeacc1a99dc52d3e
medAndro <med4ndro@gmail.com>
2025-07-24 21:37:14 +0900

    refactor: Stadium 객체 삭제와 Stadium ID 상수로 API 호출

--- android/app/src/main/java/com/yagubogu/presentation/stats/stadium/StadiumStatsViewModel.kt
+++ android/app/src/main/java/com/yagubogu/presentation/stats/stadium/StadiumStatsViewModel.kt
@@ -6,13 +6,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yagubogu.R
-import com.yagubogu.domain.model.Coordinate
-import com.yagubogu.domain.model.Latitude
-import com.yagubogu.domain.model.Longitude
 import com.yagubogu.domain.model.Team
 import com.yagubogu.domain.model.TeamOccupancyRate
 import com.yagubogu.domain.repository.StatsRepository
-import com.yagubogu.presentation.home.model.Stadium
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
@@ -21,22 +17,10 @@ import java.time.LocalDate
 class StadiumStatsViewModel(
     private val statsRepository: StatsRepository,
 ) : ViewModel() {
-    private val _currentStadium: MutableLiveData<Stadium> = MutableLiveData()
-    val currentStadium: LiveData<Stadium> get() = _currentStadium
-
     private val _stadiumStatsUiModel: MutableLiveData<StadiumStatsUiModel> = MutableLiveData()
     val stadiumStatsUiModel: LiveData<StadiumStatsUiModel> get() = _stadiumStatsUiModel
 
     init {
-        _currentStadium.value =
-            Stadium(
-                2,
-                "잠실야구장",
-                "잠실구장",
-                "잠실",
-                Coordinate(Latitude(37.5121), Longitude(127.0427)),
-            )
-
         _stadiumStatsUiModel.value =
             StadiumStatsUiModel(
                 "로딩중",
@@ -45,7 +29,8 @@ class StadiumStatsViewModel(
                 ),
             )
 
-        currentStadium.value?.id?.let { fetchStadiumStats(it, LocalDate.of(2025, 7, 25)) }
+        val today = LocalDate.now()
+        fetchStadiumStats(DUMMY_STADIUM_ID, today)
     }
 
     private fun fetchStadiumStats(
@@ -77,14 +62,11 @@ class StadiumStatsViewModel(
                     }
                 val refinedTeamStatus = refineTeamStatus(teamStatus)
 
-                currentStadium.value
-                    ?.shortName
-                    ?.let { currentStadiumShortName: String ->
-                        StadiumStatsUiModel(
-                            currentStadiumShortName,
-                            refinedTeamStatus,
-                        )
-                    }.let { _stadiumStatsUiModel.value = it }
+                _stadiumStatsUiModel.value =
+                    StadiumStatsUiModel(
+                        "잠실구장",
+                        refinedTeamStatus,
+                    )
             } else {
                 Log.e(TAG, "API 호출 실패: ${stadiumOccupancyResult.exceptionOrNull()?.message}")
             }
@@ -114,6 +96,7 @@ class StadiumStatsViewModel(
     }
 
     companion object {
+        private const val DUMMY_STADIUM_ID = 2L // 잠실구장
         private const val TAG = "StadiumStatsViewModel"
     }
 }
