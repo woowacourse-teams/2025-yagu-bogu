8777cfca95e01c5f271354738639c6a6c0b8784f
medAndro <med4ndro@gmail.com>
2025-08-01 00:17:48 +0900

    feat: 구글 로그인 버튼 클릭시 메인 액티비티 임시 연결

--- android/app/src/main/java/com/yagubogu/presentation/login/LoginActivity.kt
+++ android/app/src/main/java/com/yagubogu/presentation/login/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.yagubogu.presentation.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.yagubogu.databinding.ActivityLoginBinding
+import com.yagubogu.presentation.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
@@ -15,6 +17,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupView()
+        setupListeners()
     }
 
     private fun setupView() {
@@ -24,6 +27,13 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun setupListeners() {
+        binding.clBtnGoogle.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
