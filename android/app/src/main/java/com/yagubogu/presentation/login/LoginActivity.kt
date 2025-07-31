92d8ef730652ad4376487f319954390b158b8ec9
medAndro <med4ndro@gmail.com>
2025-08-01 00:13:24 +0900

    feat: 로그인 UI 구현 및 앱 시작 화면 설정

---
+++ android/app/src/main/java/com/yagubogu/presentation/login/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.yagubogu.presentation.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yagubogu.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.clActivityLoginRoot) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
