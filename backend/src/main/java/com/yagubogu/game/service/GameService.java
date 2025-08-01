dc08a060d46540bc675d48ab1532b0d034ede60b
juno <wnsgh732@gmail.com>
2025-08-01 11:22:15 +0900

    feat: stadium 조회 시 shortName으로 조회하도록 수정

--- backend/src/main/java/com/yagubogu/game/service/GameService.java
+++ backend/src/main/java/com/yagubogu/game/service/GameService.java
@@ -59,7 +59,7 @@ public class GameService {
     }
 
     private Stadium getStadiumByName(final String stadiumName) {
-        return stadiumRepository.findStadiumByName(STADIUM_NAME_MAP.get(stadiumName))
+        return stadiumRepository.findByShortName(STADIUM_NAME_MAP.get(stadiumName))
                 .orElseThrow(() -> new KboClientException("stadium name match failed: " + stadiumName));
     }
 
--- backend/src/main/java/com/yagubogu/stadium/repository/StadiumRepository.java
+++ backend/src/main/java/com/yagubogu/stadium/repository/StadiumRepository.java
@@ -3,14 +3,10 @@ package com.yagubogu.stadium.repository;
 import com.yagubogu.stadium.domain.Stadium;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StadiumRepository extends JpaRepository<Stadium, Long> {
 
-    @Query("SELECT s FROM Stadium s WHERE s.location = :name OR s.shortName = :name OR s.fullName = :name")
-    Optional<Stadium> findStadiumByName(@Param("name") String name);
-
+    Optional<Stadium> findByShortName(String shortName);
 }
