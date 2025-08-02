98d0e1f864fbd4a51b73602e48fcd5fe4402829a
JunHyuk Im <imjh0417@gmail.com>
2025-08-02 17:28:44 +0900

    feat: 응원팀 선택 화면 RecyclerView 구현

---
+++ android/app/src/main/java/com/yagubogu/presentation/favorite/FavoriteTeamViewHolder.kt
@@ -0,0 +1,22 @@
+package com.yagubogu.presentation.favorite
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yagubogu.databinding.ItemFavoriteTeamBinding
+
+class FavoriteTeamViewHolder(
+    private val binding: ItemFavoriteTeamBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(favoriteTeamUiModel: FavoriteTeamUiModel) {
+        binding.favoriteTeamUiModel = favoriteTeamUiModel
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): FavoriteTeamViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemFavoriteTeamBinding.inflate(inflater, parent, false)
+            return FavoriteTeamViewHolder(binding)
+        }
+    }
+}
