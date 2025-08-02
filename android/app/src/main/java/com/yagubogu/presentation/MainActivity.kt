86a27981f214e9e20845a845db3181de07a90ead
medAndro <med4ndro@gmail.com>
2025-08-02 16:32:36 +0900

    feat: 스플래시 의존성 추가 api 31+ 구현

--- android/app/src/main/java/com/yagubogu/presentation/MainActivity.kt
+++ android/app/src/main/java/com/yagubogu/presentation/MainActivity.kt
@@ -1,34 +1,45 @@
 package com.yagubogu.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import androidx.lifecycle.lifecycleScope
 import com.yagubogu.R
 import com.yagubogu.databinding.ActivityMainBinding
 import com.yagubogu.presentation.challenge.ChallengeFragment
 import com.yagubogu.presentation.home.HomeFragment
 import com.yagubogu.presentation.record.RecordFragment
 import com.yagubogu.presentation.stats.StatsFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private var isReady = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setupSplash()
         super.onCreate(savedInstanceState)
         setupView()
         setupBottomNavigationView()
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
-        if (savedInstanceState == null) {
-            binding.bnvNavigation.selectedItemId = R.id.item_home
-        }
+        performInitialization(savedInstanceState)
+    }
+
+    fun setupSplash() {
+        // Todo : android 12 이상만 지원하므로 minsdk지원 필요
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { !isReady }
     }
 
     private fun setupView() {
@@ -81,5 +92,21 @@ class MainActivity : AppCompatActivity() {
         @StringRes titleResId: Int,
     ) {
         binding.tvToolbarTitle.text = getString(titleResId)
+    }
+
+    private fun performInitialization(savedInstanceState: Bundle?) {
+        lifecycleScope.launch {
+            try {
+                // todo: 초기화 작업들
+                delay(700)
+            } catch (e: Exception) {
+                Log.e("MainActivity", "초기화 실패", e)
+            } finally {
+                isReady = true
+                if (savedInstanceState == null) {
+                    binding.bnvNavigation.selectedItemId = R.id.item_home
+                }
+            }
+        }
     }
 }
