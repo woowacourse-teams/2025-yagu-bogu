e5f86f3096befcf6e252c82bd40643f798a109da
Mint <audwl03071@gmail.com>
2025-08-04 00:05:55 +0900

    feat: Local용 AuthGateway 구현
    - 로컬에서 실행할 때 토큰을 얻어오는 mock 객체를 만든다.

--- backend/src/main/java/com/yagubogu/auth/client/GoogleAuthGateway.java
+++ backend/src/main/java/com/yagubogu/auth/client/GoogleAuthGateway.java
@@ -6,11 +6,11 @@ import com.yagubogu.auth.dto.GoogleAuthResponse;
 import com.yagubogu.auth.dto.LoginRequest;
 import com.yagubogu.auth.exception.GoogleAuthExceptionHandler;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
-@EnableConfigurationProperties(GoogleAuthProperties.class)
+@Profile("!local")
 @Component
 public class GoogleAuthGateway implements AuthGateway {
 
