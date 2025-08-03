9d22bcf92dde277e38fc8b01c4a07291b5b4074e
bowook <mbosx316@naver.com>
2025-08-04 01:13:46 +0900

    style: 개행 추가

--- backend/src/main/java/com/yagubogu/global/exception/UnprocessableEntityException.java
+++ backend/src/main/java/com/yagubogu/global/exception/UnprocessableEntityException.java
@@ -1,6 +1,7 @@
 package com.yagubogu.global.exception;
 
 public class UnprocessableEntityException extends YaguBoguException {
+
     public UnprocessableEntityException(final String message) {
         super(message);
     }
