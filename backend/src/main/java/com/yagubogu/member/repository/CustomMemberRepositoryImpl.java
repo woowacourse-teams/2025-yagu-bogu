639aeebdf1e2d7448a531055f0091dd4afb2fc7e
Mint <audwl03071@gmail.com>
2025-08-04 01:45:12 +0900

    fix: @Transactional을 붙여 더티체킹 정상적으로 수행되도록 수정

--- backend/src/main/java/com/yagubogu/member/repository/CustomMemberRepositoryImpl.java
+++ backend/src/main/java/com/yagubogu/member/repository/CustomMemberRepositoryImpl.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Repository
@@ -12,6 +13,7 @@ public class CustomMemberRepositoryImpl implements CustomMemberRepository {
 
     private final EntityManager em;
 
+    @Transactional
     @Override
     public void softDeleteById(Long id) {
         Member member = em.find(Member.class, id);
@@ -20,6 +22,7 @@ public class CustomMemberRepositoryImpl implements CustomMemberRepository {
         }
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Optional<Member> findActiveById(Long id) {
         String jpql = "SELECT m FROM Member m WHERE m.id = :id AND m.deleted = false";
