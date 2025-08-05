46bf9660b911a060620ff0cc879a3801ead65755
jiyunlee <02leejiyun@naver.com>
2025-08-05 21:15:19 +0900

    chore: 안드로이드 앱 버전 0.1.0로 변경

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
