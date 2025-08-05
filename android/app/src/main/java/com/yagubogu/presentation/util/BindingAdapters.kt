d87a19646e0df9e5042b53e82e17fef56f6df35f
medAndro <med4ndro@gmail.com>
2025-08-05 16:52:14 +0900

    feat: 현장톡 채팅 아이템 레이아웃 및 리소스 추가

--- android/app/src/main/java/com/yagubogu/presentation/util/BindingAdapters.kt
+++ android/app/src/main/java/com/yagubogu/presentation/util/BindingAdapters.kt
@@ -1,6 +1,8 @@
 package com.yagubogu.presentation.util
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.ColorRes
@@ -70,3 +72,27 @@ fun TextView.setDateFormat(date: LocalDate?) {
     date ?: return
     text = date.format(DateFormatter.yyyyMMdd)
 }
+
+@BindingAdapter("conditionalMarginStart")
+fun View.setConditionalMarginStart(marginStartDp: Int?) {
+    val layoutParams = this.layoutParams as? ViewGroup.MarginLayoutParams ?: return
+
+    marginStartDp?.let {
+        layoutParams.marginStart = it.dpToPx(context)
+    }
+
+    this.layoutParams = layoutParams
+}
+
+@BindingAdapter("conditionalMarginEnd")
+fun View.setConditionalMargins(marginEndDp: Int?) {
+    val layoutParams = this.layoutParams as? ViewGroup.MarginLayoutParams ?: return
+
+    marginEndDp?.let {
+        layoutParams.marginEnd = it.dpToPx(context)
+    }
+
+    this.layoutParams = layoutParams
+}
+
+fun Int.dpToPx(context: Context): Int = (this * context.resources.displayMetrics.density).toInt()
