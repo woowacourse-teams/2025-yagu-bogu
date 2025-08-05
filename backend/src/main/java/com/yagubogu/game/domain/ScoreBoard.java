751d1881640c5537322650c1bc141e5aaf321c22
juno <wnsgh732@gmail.com>
2025-08-05 19:35:58 +0900

    refactor: ScoreBoard에 컬럼 명시 제거

--- backend/src/main/java/com/yagubogu/game/domain/ScoreBoard.java
+++ backend/src/main/java/com/yagubogu/game/domain/ScoreBoard.java
@@ -1,6 +1,5 @@
 package com.yagubogu.game.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -9,21 +8,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @EqualsAndHashCode
+@AllArgsConstructor
 @Getter
 @Embeddable
 public class ScoreBoard {
 
-    @Column(name = "runs", nullable = true)
     private Integer runs;
-
-    @Column(name = "hits", nullable = true)
     private Integer hits;
-
-    @Column(name = "errors", nullable = true)
     private Integer errors;
-
-    @Column(name = "bases_on_balls", nullable = true)
     private Integer basesOnBalls;
 }
