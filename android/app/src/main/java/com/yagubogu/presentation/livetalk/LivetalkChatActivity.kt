032f9285451f5c816d9bf5e2499608b955aa3822
medAndro <med4ndro@gmail.com>
2025-08-05 18:48:49 +0900

    feat: 현장톡 채팅 액티비티 추가

---
+++ android/app/src/main/java/com/yagubogu/presentation/livetalk/LivetalkChatActivity.kt
@@ -0,0 +1,21 @@
+package com.yagubogu.presentation.livetalk
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yagubogu.R
+
+class LivetalkChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_livetalk_chat)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
