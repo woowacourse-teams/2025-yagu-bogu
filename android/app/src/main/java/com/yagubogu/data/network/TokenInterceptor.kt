42d8c1681c056bc93b60d47b92a735b60af6c4d7
jiyunlee <02leejiyun@naver.com>
2025-08-06 17:51:31 +0900

    feat: 토큰 interceptor 구현

---
+++ android/app/src/main/java/com/yagubogu/data/network/TokenInterceptor.kt
@@ -0,0 +1,30 @@
+package com.yagubogu.data.network
+
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+class TokenInterceptor(
+    private val tokenManager: TokenManager,
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken: String? = runBlocking { tokenManager.getAccessToken() }
+        val request: Request =
+            if (accessToken != null) {
+                chain
+                    .request()
+                    .newBuilder()
+                    .addHeader(HEADER_AUTHORIZATION, "$HEADER_AUTHORIZATION_TYPE $accessToken")
+                    .build()
+            } else {
+                chain.request()
+            }
+        return chain.proceed(request)
+    }
+
+    companion object {
+        private const val HEADER_AUTHORIZATION = "Authorization"
+        private const val HEADER_AUTHORIZATION_TYPE = "Bearer"
+    }
+}
