2bc271a99751ae719ea79a211e3457275f198d29
Mint <audwl03071@gmail.com>
2025-08-06 20:08:57 +0900

    test: 변수 추출

--- backend/src/test/java/com/yagubogu/auth/support/AuthorizationExtractorTest.java
+++ backend/src/test/java/com/yagubogu/auth/support/AuthorizationExtractorTest.java
@@ -5,10 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.context.request.ServletWebRequest;
 
 class AuthorizationExtractorTest {
+
+    private static final String BEARER = "Bearer ";
 
     private final AuthorizationExtractor authorizationExtractor = new AuthorizationExtractor();
 
@@ -17,14 +20,15 @@ class AuthorizationExtractorTest {
     void extract_NativeWebRequest() {
         // given
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader("Authorization", "Bearer abc.def.ghi");
+        String token = "abc.def.ghi";
+        request.addHeader(HttpHeaders.AUTHORIZATION, BEARER + token);
         ServletWebRequest webRequest = new ServletWebRequest(request);
 
         // when
-        Optional<String> token = authorizationExtractor.extract(webRequest);
+        Optional<String> extractedToken = authorizationExtractor.extract(webRequest);
 
         // then
-        assertThat(token).contains("abc.def.ghi");
+        assertThat(extractedToken).contains(token);
     }
 
     @DisplayName("NativeWebRequest의 헤더가 없는 경우 빈 객체를 반환한다")
@@ -35,10 +39,10 @@ class AuthorizationExtractorTest {
         ServletWebRequest webRequest = new ServletWebRequest(request);
 
         // when
-        Optional<String> token = authorizationExtractor.extract(webRequest);
+        Optional<String> extractedToken = authorizationExtractor.extract(webRequest);
 
         // then
-        assertThat(token).isEmpty();
+        assertThat(extractedToken).isEmpty();
     }
 
     @DisplayName("NativeWebRequest가 Bearer 접두어가 아닌 경우 빈 객체를 반환한다")
@@ -46,14 +50,15 @@ class AuthorizationExtractorTest {
     void extract_NativeWebRequest_not_bearer() {
         // given
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader("Authorization", "Token abc");
+        String invalidToken = "Token abc";
+        request.addHeader(HttpHeaders.AUTHORIZATION, invalidToken);
         ServletWebRequest webRequest = new ServletWebRequest(request);
 
         // when
-        Optional<String> token = authorizationExtractor.extract(webRequest);
+        Optional<String> extractedToken = authorizationExtractor.extract(webRequest);
 
         // then
-        assertThat(token).isEmpty();
+        assertThat(extractedToken).isEmpty();
     }
 
     @DisplayName("HttpServletRequest의 토큰 헤더를 추출한다")
@@ -61,13 +66,14 @@ class AuthorizationExtractorTest {
     void extract_HttpServletRequest() {
         // given
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader("Authorization", "Bearer abc.def.ghi");
+        String token = "abc.def.ghi";
+        request.addHeader(HttpHeaders.AUTHORIZATION, BEARER + token);
 
         // when
-        Optional<String> token = authorizationExtractor.extract(request);
+        Optional<String> extractedToken = authorizationExtractor.extract(request);
 
         // then
-        assertThat(token).contains("abc.def.ghi");
+        assertThat(extractedToken).contains(token);
     }
 
     @DisplayName("HttpServletRequest의 헤더가 없는 경우 빈 객체를 반환한다")
@@ -77,10 +83,10 @@ class AuthorizationExtractorTest {
         MockHttpServletRequest request = new MockHttpServletRequest();
 
         // when
-        Optional<String> token = authorizationExtractor.extract(request);
+        Optional<String> extractedToken = authorizationExtractor.extract(request);
 
         // then
-        assertThat(token).isEmpty();
+        assertThat(extractedToken).isEmpty();
     }
 
     @DisplayName("HttpServletRequest가 Bearer 접두어가 아닌 경우 빈 객체를 반환한다")
@@ -88,13 +94,14 @@ class AuthorizationExtractorTest {
     void extract_HttpServletRequest_not_bearer() {
         // given
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader("Authorization", "Token abc");
+        String invalidToken = "Token abc";
+        request.addHeader(HttpHeaders.AUTHORIZATION, invalidToken);
 
         // when
-        Optional<String> token = authorizationExtractor.extract(request);
+        Optional<String> extractedToken = authorizationExtractor.extract(request);
 
         // then
-        assertThat(token).isEmpty();
+        assertThat(extractedToken).isEmpty();
     }
 
     @DisplayName("HttpServletRequest에 여러 Authorization 헤더가 있을 때 Bearer 토큰을 추출한다")
@@ -102,13 +109,14 @@ class AuthorizationExtractorTest {
     void extract_HttpServletRequest_multiple_headers() {
         // given
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.addHeader("Authorization", "Token xyz");
-        request.addHeader("Authorization", "Bearer abc.def.ghi");
+        request.addHeader(HttpHeaders.AUTHORIZATION, "Token xyz");
+        String token = "abc.def.ghi";
+        request.addHeader(HttpHeaders.AUTHORIZATION, BEARER + token);
 
         // when
-        Optional<String> token = authorizationExtractor.extract(request);
+        Optional<String> extractedToken = authorizationExtractor.extract(request);
 
         // then
-        assertThat(token).contains("abc.def.ghi");
+        assertThat(extractedToken).contains(token);
     }
 }
