265d68c40c61d69088ebb31f57dc91ba720e2009
JunHyuk Im <imjh0417@gmail.com>
2025-08-07 10:33:24 +0900

    refactor: 현장톡 구장 선택 클릭 리스너 Activity에서 구현 후 전달

--- android/app/src/main/java/com/yagubogu/presentation/livetalk/stadium/LivetalkStadiumViewHolder.kt
+++ android/app/src/main/java/com/yagubogu/presentation/livetalk/stadium/LivetalkStadiumViewHolder.kt
@@ -1,26 +1,30 @@
 package com.yagubogu.presentation.livetalk.stadium
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.yagubogu.databinding.ItemLivetalkStadiumBinding
-import com.yagubogu.presentation.livetalk.LivetalkChatActivity
 
 class LivetalkStadiumViewHolder(
     private val binding: ItemLivetalkStadiumBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
+    init {
+        binding.constraintLivetalkStadium
+    }
+
     fun bind(item: LivetalkStadiumItem) {
         binding.livetalkStadiumItem = item
-        binding.constraintLivetalkStadium.setOnClickListener {
-            // Todo: 구장별 채팅 연동 필요
-            val intent = Intent(binding.root.context, LivetalkChatActivity::class.java)
-            binding.root.context.startActivity(intent)
-        }
+    }
+
+    interface Handler {
+        fun onItemClick(item: LivetalkStadiumItem)
     }
 
     companion object {
-        fun from(parent: ViewGroup): LivetalkStadiumViewHolder {
+        fun from(
+            parent: ViewGroup,
+            handler: Handler,
+        ): LivetalkStadiumViewHolder {
             val inflater = LayoutInflater.from(parent.context)
             val binding = ItemLivetalkStadiumBinding.inflate(inflater, parent, false)
             return LivetalkStadiumViewHolder(binding)
