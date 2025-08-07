97135e83fde339ba3e4ae7daed3e48e34d706cc2
JunHyuk Im <imjh0417@gmail.com>
2025-08-07 19:22:57 +0900

    fix: nextCursorId의 타입을 Int?로 변경

--- android/app/src/main/java/com/yagubogu/data/dto/response/talks/TalksResponse.kt
+++ android/app/src/main/java/com/yagubogu/data/dto/response/talks/TalksResponse.kt
@@ -8,7 +8,7 @@ data class TalksResponse(
     @SerialName("content")
     val contents: List<ContentDto>, // 페이징된 톡 메시지 리스트
     @SerialName("nextCursorId")
-    val nextCursorId: Int, // 다음 페이지 조회를 위한 커서 ID (없으면 null)
+    val nextCursorId: Int?, // 다음 페이지 조회를 위한 커서 ID (없으면 null)
     @SerialName("hasNext")
     val hasNext: Boolean, // 다음 페이지가 존재하는지 여부
 )
