40943b8aa786f4207696afe0c70b6823255c2af0
medAndro <med4ndro@gmail.com>
2025-08-07 10:21:28 +0900

    refactor: 현장톡 말풍선 UI 분리

---
+++ android/app/src/main/java/com/yagubogu/presentation/livetalk/chat/LivetalkMyBubbleViewHolder.kt
@@ -0,0 +1,22 @@
+package com.yagubogu.presentation.livetalk.chat
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yagubogu.databinding.ItemLivetalkMyBubbleBinding
+
+class LivetalkMyBubbleViewHolder(
+    private val binding: ItemLivetalkMyBubbleBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: LivetalkChatItem) {
+        binding.livetalkChatItem = item
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): LivetalkMyBubbleViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemLivetalkMyBubbleBinding.inflate(inflater, parent, false)
+            return LivetalkMyBubbleViewHolder(binding)
+        }
+    }
+}
---
+++ android/app/src/main/java/com/yagubogu/presentation/livetalk/chat/LivetalkOtherBubbleViewHolder.kt
@@ -0,0 +1,22 @@
+package com.yagubogu.presentation.livetalk.chat
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yagubogu.databinding.ItemLivetalkOtherBubbleBinding
+
+class LivetalkOtherBubbleViewHolder(
+    private val binding: ItemLivetalkOtherBubbleBinding,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: LivetalkChatItem) {
+        binding.livetalkChatItem = item
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): LivetalkOtherBubbleViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemLivetalkOtherBubbleBinding.inflate(inflater, parent, false)
+            return LivetalkOtherBubbleViewHolder(binding)
+        }
+    }
+}
