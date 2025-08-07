7d6f337429d86a681e1b886f1c8c1e7ce18ecaa5
jiyunlee <02leejiyun@naver.com>
2025-08-08 00:50:50 +0900

    refactor: 팬 비율 계산 로직 개선

--- android/app/src/main/java/com/yagubogu/presentation/home/stadium/StadiumFanRateItem.kt
+++ android/app/src/main/java/com/yagubogu/presentation/home/stadium/StadiumFanRateItem.kt
@@ -4,18 +4,25 @@ data class StadiumFanRateItem(
     val awayTeamFanRate: TeamFanRate,
     val homeTeamFanRate: TeamFanRate,
 ) {
-    val awayTeamPercentage: Double =
-        awayTeamFanRate.fanRate / (awayTeamFanRate.fanRate + homeTeamFanRate.fanRate) * FULL_PERCENTAGE
-    val homeTeamPercentage: Double =
-        homeTeamFanRate.fanRate / (awayTeamFanRate.fanRate + homeTeamFanRate.fanRate) * FULL_PERCENTAGE
+    val awayTeamPercentage: Double = calculatePercentage(awayTeamFanRate.fanRate)
+    val homeTeamPercentage: Double = calculatePercentage(homeTeamFanRate.fanRate)
 
-    val awayTeamBias: Double = remapRange(awayTeamPercentage)
-    val homeTeamBias: Double = remapRange(homeTeamPercentage)
+    val awayTeamChartRange: Double = remapToChartRange(awayTeamPercentage)
+    val homeTeamChartRange: Double = remapToChartRange(homeTeamPercentage)
 
-    private fun remapRange(value: Double): Double {
+    private fun calculatePercentage(fanRate: Double): Double {
+        val totalFanRate: Double = awayTeamFanRate.fanRate + homeTeamFanRate.fanRate
+        return if (totalFanRate == 0.0) {
+            FULL_PERCENTAGE / 2
+        } else {
+            fanRate / totalFanRate * FULL_PERCENTAGE
+        }
+    }
+
+    private fun remapToChartRange(percentage: Double): Double {
         val scalingFactor: Double = (FULL_PERCENTAGE - CHART_END_PADDING_SIZE * 2) / FULL_PERCENTAGE
-        val percentResult: Double = CHART_END_PADDING_SIZE + value * scalingFactor
-        return percentResult / FULL_PERCENTAGE
+        val scaledRange: Double = CHART_END_PADDING_SIZE + percentage * scalingFactor
+        return scaledRange / FULL_PERCENTAGE
     }
 
     companion object {
