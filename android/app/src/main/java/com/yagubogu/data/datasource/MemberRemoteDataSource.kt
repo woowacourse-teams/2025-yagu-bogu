bd2614348995c1a6cbc813844b219ad9c349da86
jiyunlee <02leejiyun@naver.com>
2025-08-07 19:12:55 +0900

    feat: 응원팀 수정 API 추가

--- android/app/src/main/java/com/yagubogu/data/datasource/MemberRemoteDataSource.kt
+++ android/app/src/main/java/com/yagubogu/data/datasource/MemberRemoteDataSource.kt
@@ -1,8 +1,10 @@
 package com.yagubogu.data.datasource
 
+import com.yagubogu.data.dto.request.MemberFavoriteRequest
 import com.yagubogu.data.dto.response.MemberFavoriteResponse
 import com.yagubogu.data.service.MemberApiService
 import com.yagubogu.data.util.safeApiCall
+import com.yagubogu.domain.model.Team
 
 class MemberRemoteDataSource(
     private val memberApiService: MemberApiService,
@@ -10,5 +12,11 @@ class MemberRemoteDataSource(
     override suspend fun getFavoriteTeam(): Result<MemberFavoriteResponse> =
         safeApiCall {
             memberApiService.getFavoriteTeam()
+        }
+
+    override suspend fun updateFavoriteTeam(team: Team): Result<MemberFavoriteResponse> =
+        safeApiCall {
+            val request = MemberFavoriteRequest(team.name)
+            memberApiService.patchFavoriteTeam(request)
         }
 }
--- android/app/src/main/java/com/yagubogu/data/service/MemberApiService.kt
+++ android/app/src/main/java/com/yagubogu/data/service/MemberApiService.kt
@@ -1,10 +1,18 @@
 package com.yagubogu.data.service
 
+import com.yagubogu.data.dto.request.MemberFavoriteRequest
 import com.yagubogu.data.dto.response.MemberFavoriteResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 
 interface MemberApiService {
     @GET("/api/members/favorites")
     suspend fun getFavoriteTeam(): Response<MemberFavoriteResponse>
+
+    @PATCH("/api/members/favorites")
+    suspend fun patchFavoriteTeam(
+        @Body body: MemberFavoriteRequest,
+    ): Response<MemberFavoriteResponse>
 }
