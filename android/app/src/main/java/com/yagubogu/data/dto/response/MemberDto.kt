7dbb65e3d48b1ad858c2ad7d3cc8415547eefa04
jiyunlee <02leejiyun@naver.com>
2025-08-12 22:40:58 +0900

    refactor: Member id 타입을 Long으로 변경

--- android/app/src/main/java/com/yagubogu/data/dto/response/MemberDto.kt
+++ android/app/src/main/java/com/yagubogu/data/dto/response/MemberDto.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MemberDto(
     @SerialName("id")
-    val id: Int, // 사용자 id
+    val id: Long, // 사용자 id
     @SerialName("nickname")
     val nickname: String, // 사용자 닉네임
     @SerialName("profileImageUrl")
