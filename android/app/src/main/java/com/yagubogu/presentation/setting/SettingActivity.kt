31ba1b32264d2992b416a61d79c217eba1558f54
medAndro <med4ndro@gmail.com>
2025-08-16 20:12:38 +0900

    feat: 설정 Activity 작성

---
+++ android/app/src/main/java/com/yagubogu/presentation/setting/SettingActivity.kt
@@ -0,0 +1,29 @@
+package com.yagubogu.presentation.setting
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yagubogu.databinding.ActivitySettingBinding
+
+class SettingActivity : AppCompatActivity() {
+    private val binding: ActivitySettingBinding by lazy {
+        ActivitySettingBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.constraintActivitySettingRoot) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, 0, systemBars.right, 0)
+            insets
+        }
+    }
+}
