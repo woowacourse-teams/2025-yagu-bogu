cbe514471526e45eae4dff5087e0b86a26d2cb61
bowook <mbosx316@naver.com>
2025-08-17 17:30:54 +0900

    chore: prod ci 작업 삭제

--- backend/src/main/java/com/yagubogu/YaguboguApplication.java
+++ backend/src/main/java/com/yagubogu/YaguboguApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YaguboguApplication {
 
     public static void main(String[] args) {
-        System.out.println("?");
         SpringApplication.run(YaguboguApplication.class, args);
     }
 }
