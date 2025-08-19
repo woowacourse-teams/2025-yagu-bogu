43d208ee2c46a34ac24bc749fe56b62fb13698b1
medAndro <med4ndro@gmail.com>
2025-08-19 12:05:18 +0900

    feat: 기본 다이얼로그 UI 구현

---
+++ android/app/src/main/java/com/yagubogu/presentation/util/DialogDefaultUiModel.kt
@@ -0,0 +1,8 @@
+package com.yagubogu.presentation.util
+
+data class DialogDefaultUiModel(
+    val title: String,
+    val message: String,
+    val negativeText: String?,
+    val positiveText: String?,
+)
