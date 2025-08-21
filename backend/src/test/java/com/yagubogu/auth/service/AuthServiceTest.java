dfa3faa588e1b37e649e541bdabd3bf20cc57594
Mint <audwl03071@gmail.com>
2025-08-22 05:32:36 +0900

    fix: soft delete member 쿼리 수정

--- backend/src/test/java/com/yagubogu/auth/service/AuthServiceTest.java
+++ backend/src/test/java/com/yagubogu/auth/service/AuthServiceTest.java
@@ -1,5 +1,8 @@
 package com.yagubogu.auth.service;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
 import com.yagubogu.auth.config.AuthTestConfig;
 import com.yagubogu.auth.config.AuthTokenProperties;
 import com.yagubogu.auth.domain.RefreshToken;
@@ -26,9 +29,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 @DataJpaTest
 @Import({AuthTestConfig.class, JpaAuditingConfig.class})
