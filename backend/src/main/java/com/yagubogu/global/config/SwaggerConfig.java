999942d6b76ee5093d3220b88ac90b05ad3feeb7
nourzoo <tmddus4671@gmail.com>
2025-08-21 11:22:32 +0900

    chore: 프로필별 baseurl 수정

--- backend/src/main/java/com/yagubogu/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/yagubogu/global/config/SwaggerConfig.java
@@ -5,15 +5,26 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.stream.Collectors;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@EnableConfigurationProperties(SwaggerUiServersProperties.class)
 @Configuration
 public class SwaggerConfig {
 
+
+    private final SwaggerUiServersProperties serversProps;
+
+    public SwaggerConfig(SwaggerUiServersProperties serversProps) {
+        this.serversProps = serversProps;
+    }
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .components(new Components().addSecuritySchemes("Access Token",
                         new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
@@ -21,6 +32,17 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList("Access Token"))
                 .info(apiInfo());
+
+        // application.yml의 springdoc.swagger-ui.servers를 OpenAPI servers에 반영
+        if (serversProps.getServers() != null && !serversProps.getServers().isEmpty()) {
+            openAPI.setServers(
+                    serversProps.getServers().stream()
+                            .map(s -> new Server().url(s.getUrl()).description(s.getName()))
+                            .collect(Collectors.toList())
+            );
+        }
+
+        return openAPI;
     }
 
     private Info apiInfo() {
