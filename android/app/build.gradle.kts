d78d4e286a3a4edd2a7df3c4d806d7ff0f298ffc
jiyunlee <02leejiyun@naver.com>
2025-08-22 16:04:52 +0900

    build: 안드로이드 앱 버전 정보 업데이트
    - versionCode: 3 -> 4
    - versionName: 0.3.0 -> 1.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.yagubogu"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
