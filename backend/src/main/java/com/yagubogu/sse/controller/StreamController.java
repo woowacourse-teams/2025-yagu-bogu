865c6f23bc6b59b86ab309d18294ab45b090e448
nourzoo <tmddus4671@gmail.com>
2025-09-08 16:47:24 +0900

    feat: SseEmitterService 구현

--- backend/src/main/java/com/yagubogu/sse/controller/StreamController.java
+++ backend/src/main/java/com/yagubogu/sse/controller/StreamController.java
@@ -1,17 +1,22 @@
 package com.yagubogu.sse.controller;
 
+import com.yagubogu.sse.service.SseEmitterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/event-stream")
 public class StreamController {
 
+    private final SseEmitterService sseEmitterService;
 
     @GetMapping
     public ResponseEntity<Void> getEventStream() {
+        SseEmitter emitter = sseEmitterService.add();
+
 
     }
 }
---
+++ backend/src/main/java/com/yagubogu/sse/service/SseEmitterService.java
@@ -0,0 +1,22 @@
+package com.yagubogu.sse.service;
+
+import com.yagubogu.sse.repository.SseEmitterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RequiredArgsConstructor
+@Service
+public class SseEmitterService {
+
+    private static final long ONE_HOUR_TIMEOUT = 60L * 60 * 1000;
+
+    private final SseEmitterRepository sseEmitterRepository;
+
+    public SseEmitter add() {
+        SseEmitter emitter = new SseEmitter(ONE_HOUR_TIMEOUT);
+        sseEmitterRepository.add(emitter);
+
+
+    }
+}
