cf5d58198d4cc2efcfb7bcc714241843ec2e3a9d
JunHyuk Im <imjh0417@gmail.com>
2025-09-16 16:08:42 +0900

    refactor: 직관 내역 아이템 FirebaseAnalytics 적용

--- android/app/src/main/java/com/yagubogu/presentation/attendance/AttendanceHistoryFragment.kt
+++ android/app/src/main/java/com/yagubogu/presentation/attendance/AttendanceHistoryFragment.kt
@@ -9,6 +9,9 @@ import android.widget.ArrayAdapter
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
 import com.yagubogu.R
 import com.yagubogu.YaguBoguApplication
 import com.yagubogu.databinding.FragmentAttendanceHistoryBinding
@@ -17,7 +20,10 @@ import com.yagubogu.presentation.attendance.model.AttendanceHistoryItem
 import com.yagubogu.presentation.attendance.model.AttendanceHistoryOrder
 
 @Suppress("ktlint:standard:backing-property-naming")
-class AttendanceHistoryFragment : Fragment() {
+class AttendanceHistoryFragment :
+    Fragment(),
+    AttendanceHistorySummaryViewHolder.Handler,
+    AttendanceHistoryDetailViewHolder.Handler {
     private var _binding: FragmentAttendanceHistoryBinding? = null
     private val binding: FragmentAttendanceHistoryBinding get() = _binding!!
 
@@ -28,10 +34,12 @@ class AttendanceHistoryFragment : Fragment() {
 
     private val attendanceHistoryAdapter by lazy {
         AttendanceHistoryAdapter(
-            attendanceHistorySummaryHandler = viewModel,
-            attendanceHistoryDetailHandler = viewModel,
+            attendanceHistorySummaryHandler = this,
+            attendanceHistoryDetailHandler = this,
         )
     }
+
+    private val firebaseAnalytics: FirebaseAnalytics by lazy { Firebase.analytics }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -62,6 +70,16 @@ class AttendanceHistoryFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onSummaryItemClick(item: AttendanceHistoryItem.Summary) {
+        viewModel.onSummaryItemClick(item)
+        firebaseAnalytics.logEvent("attendance_history_item_click", null)
+    }
+
+    override fun onDetailItemClick(item: AttendanceHistoryItem.Detail) {
+        viewModel.onDetailItemClick(item)
+        firebaseAnalytics.logEvent("attendance_history_item_click", null)
     }
 
     private fun setupBindings() {
