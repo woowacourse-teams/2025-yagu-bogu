9053970fb4009e2501329d04a7931477bdfe0b81
jiyunlee <02leejiyun@naver.com>
2025-09-19 16:32:48 +0900

    build: 앱 버전 정보 업데이트

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1_01_00
-        versionName = "1.0.1"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
