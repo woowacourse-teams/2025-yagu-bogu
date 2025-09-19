36478af103f2d7dc3ea285b2a78e1299e9ae59fb
Mint <audwl03071@gmail.com>
2025-09-19 16:03:32 +0900

    feat: 크론시간, poolSize 변경

--- backend/src/main/java/com/yagubogu/global/config/ScheduleConfig.java
+++ backend/src/main/java/com/yagubogu/global/config/ScheduleConfig.java
@@ -1,5 +1,6 @@
 package com.yagubogu.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
@@ -8,13 +9,18 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @EnableScheduling
 @Configuration
+@Slf4j
 public class ScheduleConfig {
 
     @Bean
     public TaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(2);
+        scheduler.setPoolSize(4);
         scheduler.setThreadNamePrefix("sch-");
+        scheduler.setWaitForTasksToCompleteOnShutdown(true);
+        scheduler.setAwaitTerminationSeconds(30);
+        scheduler.setRemoveOnCancelPolicy(true);
+        scheduler.setErrorHandler(t -> log.error("Scheduled task error", t));
         scheduler.initialize();
         return scheduler;
     }
