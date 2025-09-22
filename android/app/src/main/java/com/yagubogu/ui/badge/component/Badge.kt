a142bc1d3a2d71d2698f37f1abe127e510658dbf
jiyunlee <02leejiyun@naver.com>
2025-09-22 15:39:50 +0900

    feat: 배지 컴포넌트 구현

---
+++ android/app/src/main/java/com/yagubogu/ui/badge/component/Badge.kt
@@ -0,0 +1,48 @@
+package com.yagubogu.ui.badge.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yagubogu.R
+import com.yagubogu.ui.theme.PretendardSemiBold
+
+@Composable
+fun Badge(
+    @DrawableRes image: Int,
+    name: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(image),
+            contentDescription = null,
+            modifier = Modifier.size(120.dp),
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = name,
+            style = PretendardSemiBold,
+            fontSize = 14.sp,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BadgePreview() {
+    Badge(image = R.drawable.img_badge_princess, name = "공포의 주둥아리")
+}
--- android/app/src/main/java/com/yagubogu/ui/theme/Type.kt
+++ android/app/src/main/java/com/yagubogu/ui/theme/Type.kt
@@ -4,9 +4,16 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-val PretendardBold20 =
+val PretendardSemiBold =
+    TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.SemiBold,
+    )
+
+val PretendardBold =
     TextStyle(
         fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.Bold,
-        fontSize = 20.sp,
     )
+
+val PretendardBold20 = PretendardBold.copy(fontSize = 20.sp)
