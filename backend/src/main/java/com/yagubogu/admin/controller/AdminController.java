d9f44fa3b64e163ccff7cc163c90c64468a1d2dd
nourzoo <tmddus4671@gmail.com>
2025-09-23 10:16:49 +0900

    feat: 어드민용 controller 추가 (game schedules sync)

--- backend/src/main/java/com/yagubogu/admin/controller/AdminController.java
+++ backend/src/main/java/com/yagubogu/admin/controller/AdminController.java
@@ -2,27 +2,40 @@ package com.yagubogu.admin.controller;
 
 import com.yagubogu.auth.annotation.RequireRole;
 import com.yagubogu.game.service.GameResultSyncService;
+import com.yagubogu.game.service.GameScheduleSyncService;
 import com.yagubogu.member.domain.Role;
+import io.swagger.v3.oas.annotations.Hidden;
 import java.time.LocalDate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RequireRole(Role.ADMIN)
 @RequestMapping("/api/admin")
 @RestController
 public class AdminController {
 
     private final GameResultSyncService gameResultSyncService;
+    private final GameScheduleSyncService gameScheduleSyncService;
 
-    public AdminController(final GameResultSyncService gameResultSyncService) {
+    public AdminController(final GameResultSyncService gameResultSyncService,
+                           final GameScheduleSyncService gameScheduleSyncService) {
         this.gameResultSyncService = gameResultSyncService;
+        this.gameScheduleSyncService = gameScheduleSyncService;
     }
 
     @PatchMapping("/game-results")
-    public ResponseEntity<Void> fetchForceDailyGameSchedule() {
+    public ResponseEntity<Void> fetchForceDailyGameResult() {
         gameResultSyncService.syncGameResult(LocalDate.now());
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/game-schedules")
+    public ResponseEntity<Void> fetchForceDailyGameSchedule() {
+        gameScheduleSyncService.syncGameSchedule(LocalDate.now());
 
         return ResponseEntity.ok().build();
     }
