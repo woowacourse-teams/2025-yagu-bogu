9d1977961a06284eccd4261a3a7f2c6c5ca41cb8
bowook <mbosx316@naver.com>
2025-09-25 15:57:39 +0900

    refactor: 2N+1 문제 해결

--- backend/src/main/java/com/yagubogu/stat/dto/LuckyStadiumResponse.java
+++ backend/src/main/java/com/yagubogu/stat/dto/LuckyStadiumResponse.java
@@ -1,14 +1,6 @@
 package com.yagubogu.stat.dto;
 
-import com.yagubogu.stadium.domain.Stadium;
-
 public record LuckyStadiumResponse(
         String shortName
 ) {
-    public static LuckyStadiumResponse from(final Stadium luckyStadium) {
-        if (luckyStadium == null) {
-            return new LuckyStadiumResponse(null);
-        }
-        return new LuckyStadiumResponse(luckyStadium.getShortName());
-    }
 }
