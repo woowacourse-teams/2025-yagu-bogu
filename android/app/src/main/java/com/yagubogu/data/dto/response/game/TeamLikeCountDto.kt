3f55985b2de262583901b0e3bae1fc6b07cddd70
ijh1298 <imjh0417@gmail.com>
2025-10-08 00:45:07 +0900

    refactor: TeamLikeCountDto의 teamId 타입을 Long에서 String으로 변경

--- android/app/src/main/java/com/yagubogu/data/dto/response/game/TeamLikeCountDto.kt
+++ android/app/src/main/java/com/yagubogu/data/dto/response/game/TeamLikeCountDto.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class TeamLikeCountDto(
     @SerialName("teamId")
-    val teamId: Long, // 팀을 구분하는 코드 (예: "SS", "LG", "OB").
+    val teamId: String, // 팀을 구분하는 코드 (예: "SS", "LG", "OB").
     @SerialName("totalCount")
     val totalCount: Int, // 해당 팀이 받은 좋아요(like)의 총 개수. 좋아요가 없으면 0.
 )
