c26fc5d4e03b0b5b589cb2e49745ee8c8c24cf5c
ijh1298 <imjh0417@gmail.com>
2025-10-10 16:34:21 +0900

    refactor: BadgeDto 불필요한 필드 제거 및 설명 주석 추가

--- android/app/src/main/java/com/yagubogu/data/dto/response/member/BadgeDto.kt
+++ android/app/src/main/java/com/yagubogu/data/dto/response/member/BadgeDto.kt
@@ -10,23 +10,21 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class BadgeDto(
     @SerialName("id")
-    val id: Long,
+    val id: Long, // 배지 ID
     @SerialName("name")
-    val name: String,
+    val name: String, // 배지 이름
     @SerialName("description")
-    val description: String,
-    @SerialName("policy")
-    val policy: String,
+    val description: String, // 배지 설명
     @SerialName("acquired")
-    val acquired: Boolean,
+    val acquired: Boolean, // 획득 여부
     @SerialName("achievedAt")
-    val achievedAt: LocalDateTime?,
+    val achievedAt: LocalDateTime?, // 획득한 날짜 및 시간
     @SerialName("badgeImageUrl")
-    val badgeImageUrl: String,
+    val badgeImageUrl: String, // 배지 이미지 주소
     @SerialName("progressRate")
-    val progressRate: Double,
+    val progressRate: Double, // 진행률(개인 사용자)
     @SerialName("achievedRate")
-    val achievedRate: Double,
+    val achievedRate: Double, // 획득률(모든 사용자 기반)
 ) {
     fun toPresentation(): BadgeInfoUiModel {
         val badge =
