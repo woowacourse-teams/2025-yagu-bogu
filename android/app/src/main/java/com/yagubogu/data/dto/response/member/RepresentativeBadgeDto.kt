48d195968ea2bc8d2cdd0d1066c5584b0bf419cf
ijh1298 <imjh0417@gmail.com>
2025-10-10 16:50:42 +0900

    refactor: RepresentativeBadgeDto 불필요한 필드 제거 및 설명 주석 추가

--- android/app/src/main/java/com/yagubogu/data/dto/response/member/RepresentativeBadgeDto.kt
+++ android/app/src/main/java/com/yagubogu/data/dto/response/member/RepresentativeBadgeDto.kt
@@ -7,13 +7,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class RepresentativeBadgeDto(
     @SerialName("id")
-    val id: Long,
+    val id: Long, // 대표 배지 ID
     @SerialName("name")
-    val name: String,
-    @SerialName("policy")
-    val policy: String,
+    val name: String, // 대표 배지 이름
     @SerialName("badgeImageUrl")
-    val badgeImageUrl: String,
+    val badgeImageUrl: String, // 대표 배지 이미지 주소
 ) {
     fun toPresentation(): BadgeUiModel =
         BadgeUiModel(
