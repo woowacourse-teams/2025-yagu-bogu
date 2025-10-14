2e7b7441542543490c18977c7936d2e2c8a600d5
bowook <mbosx316@naver.com>
2025-10-15 01:10:31 +0900

    style: 개행 추가

--- backend/src/main/java/com/yagubogu/badge/policy/BadgePolicy.java
+++ backend/src/main/java/com/yagubogu/badge/policy/BadgePolicy.java
@@ -4,5 +4,6 @@ import com.yagubogu.badge.BadgeEvent;
 import com.yagubogu.badge.dto.BadgeAwardCandidate;
 
 public interface BadgePolicy {
+    
     BadgeAwardCandidate determineAwardCandidate(BadgeEvent event);
 }
