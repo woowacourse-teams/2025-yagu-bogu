8a98fcdd4b944614283cefe49fc8d6fb502f0b93
ijh1298 <imjh0417@gmail.com>
2025-10-15 20:56:11 +0900

    refactor: 배지 이미지 로딩 상태 rememberSaveable로 변경

--- android/app/src/main/java/com/yagubogu/ui/badge/component/Badge.kt
+++ android/app/src/main/java/com/yagubogu/ui/badge/component/Badge.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,7 +35,7 @@ fun Badge(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.noRippleClickable(onClick),
     ) {
-        var isLoading by remember { mutableStateOf(true) }
+        var isLoading by rememberSaveable { mutableStateOf(true) }
 
         Box(
             modifier =
