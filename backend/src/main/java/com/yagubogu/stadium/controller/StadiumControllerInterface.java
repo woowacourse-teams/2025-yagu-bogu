f8acf24dd463752728c8c5aabc58b3fa71ddba8b
Mint <audwl03071@gmail.com>
2025-10-16 11:27:39 +0900

    docs: swagger 명세 수정

--- backend/src/main/java/com/yagubogu/stadium/controller/StadiumControllerInterface.java
+++ backend/src/main/java/com/yagubogu/stadium/controller/StadiumControllerInterface.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/stadiums")
 public interface StadiumControllerInterface {
 
-    @Operation(summary = "전체 구장 목록 조회", description = "등록된 모든 구장 목록을 조회합니다. 제 2구장은 제외됩니다.")
+    @Operation(summary = "전체 구장 목록 조회", description = "등록된 모든 구장 목록을 조회합니다. 제 2구장을 제외한 등록된 모든 구장 목록을 조회합니다.")
     @ApiResponse(responseCode = "200", description = "구장 목록 조회 성공")
     @GetMapping
     ResponseEntity<StadiumsResponse> findAllMainStadiums();
