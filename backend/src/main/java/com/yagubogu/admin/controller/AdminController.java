a55817190bda71826b0e5adccea5ef78e343f9dc
juno <wnsgh732@gmail.com>
2025-10-17 11:00:40 +0900

    style: 어노테이션 순서 변경

--- backend/src/main/java/com/yagubogu/admin/controller/AdminController.java
+++ backend/src/main/java/com/yagubogu/admin/controller/AdminController.java
@@ -15,10 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @Hidden
 @RequireRole(Role.ADMIN)
 @RequestMapping("/api/admin")
-@RequiredArgsConstructor
 @RestController
 public class AdminController {
 
