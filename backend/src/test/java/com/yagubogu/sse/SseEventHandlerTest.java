cefaea9bb8702ce1d3ee6b7ac152807909cfb415
Mint <audwl03071@gmail.com>
2025-11-17 19:33:30 +0900

    test: 테스트 수정

--- backend/src/test/java/com/yagubogu/sse/SseEventHandlerTest.java
+++ backend/src/test/java/com/yagubogu/sse/SseEventHandlerTest.java
@@ -1,33 +1,39 @@
 package com.yagubogu.sse;
 
-import com.yagubogu.checkin.service.CheckInService;
-import com.yagubogu.sse.dto.event.CheckInCreatedEvent;
-import com.yagubogu.sse.repository.SseEmitterRegistry;
-import java.time.LocalDate;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.yagubogu.checkin.cache.FanRateCache;
+import com.yagubogu.checkin.service.CheckInService;
+import com.yagubogu.sse.dto.event.CheckInCreatedEvent;
+import com.yagubogu.sse.repository.SseEmitterRegistry;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.Executor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 class SseEventHandlerTest {
 
-    private SseEmitterRegistry repository;
+    private SseEmitterRegistry sseEmitterRegistry;
     private CheckInService checkInService;
     private SseEventHandler handler;
+    private FanRateCache fanRateCache;
+    private Executor sseExecutor;
 
     @BeforeEach
     void setUp() {
-        repository = new SseEmitterRegistry();
+        sseEmitterRegistry = new SseEmitterRegistry();
         checkInService = mock(CheckInService.class);
-        handler = new SseEventHandler(repository, checkInService);
+        fanRateCache = new FanRateCache();
+        sseExecutor = Runnable::run;
+        handler = new SseEventHandler(checkInService, sseEmitterRegistry, fanRateCache, sseExecutor);
     }
 
     @DisplayName("특정 날짜로 이벤트가 들어오면 Repository의 모든 Emitter에 이벤트가 전달된다")
@@ -40,8 +46,8 @@ class SseEventHandlerTest {
 
         RecordingEmitter e1 = new RecordingEmitter();
         RecordingEmitter e2 = new RecordingEmitter();
-        repository.add(e1);
-        repository.add(e2);
+        sseEmitterRegistry.add(e1);
+        sseEmitterRegistry.add(e2);
 
         // when
         handler.onCheckInCreated(new CheckInCreatedEvent());
