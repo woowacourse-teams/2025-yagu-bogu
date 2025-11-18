fb5b17598d4ea40089c75c629fbaf17ae1fff3aa
jiyunlee <02leejiyun@naver.com>
2025-11-18 18:55:45 +0900

    refactor: GoogleModule을 별도 파일로 분리

--- android/app/src/main/java/com/yagubogu/di/AndroidModule.kt
+++ android/app/src/main/java/com/yagubogu/di/AndroidModule.kt
@@ -4,15 +4,10 @@ import android.content.ContentResolver
 import android.content.Context
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.yagubogu.BuildConfig
-import com.yagubogu.data.auth.GoogleCredentialManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -30,14 +25,4 @@ object AndroidModule {
     fun provideFusedLocationClient(
         @ApplicationContext context: Context,
     ): FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
-}
-
-@Module
-@InstallIn(ActivityComponent::class)
-object GoogleModule {
-    @Provides
-    @ActivityScoped
-    fun provideGoogleCredentialManager(
-        @ActivityContext context: Context,
-    ): GoogleCredentialManager = GoogleCredentialManager(context, BuildConfig.WEB_CLIENT_ID, "")
 }
