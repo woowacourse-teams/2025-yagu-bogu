2dd252cbab5b22050950b6c7c45f14bd0e4fc409
jiyunlee <02leejiyun@naver.com>
2025-11-21 23:40:18 +0900

    refactor: 데이터 계층에서 UI 의존성 제거

--- android/app/src/main/java/com/yagubogu/presentation/mapper/CheckInUiMapper.kt
+++ android/app/src/main/java/com/yagubogu/presentation/mapper/CheckInUiMapper.kt
@@ -1,18 +1,33 @@
 package com.yagubogu.presentation.mapper
 
 import com.yagubogu.data.dto.response.checkin.CheckInGameDto
+import com.yagubogu.data.dto.response.checkin.CheckInGameTeamDto
 import com.yagubogu.data.dto.response.checkin.FanRateByGameDto
+import com.yagubogu.data.dto.response.checkin.ScoreBoardDto
 import com.yagubogu.data.dto.response.checkin.StadiumCheckInCountDto
+import com.yagubogu.data.dto.response.checkin.TeamFanRateDto
+import com.yagubogu.domain.model.GameResult
+import com.yagubogu.domain.model.Team
 import com.yagubogu.presentation.attendance.model.AttendanceHistoryItem
+import com.yagubogu.presentation.attendance.model.GameScoreBoard
+import com.yagubogu.presentation.attendance.model.GameTeam
 import com.yagubogu.presentation.home.stadium.StadiumFanRateItem
+import com.yagubogu.presentation.home.stadium.TeamFanRate
 import com.yagubogu.presentation.stats.detail.StadiumVisitCount
 import java.time.LocalDate
 
 fun FanRateByGameDto.toUiModel(): StadiumFanRateItem =
     StadiumFanRateItem(
         gameId = gameId,
-        awayTeamFanRate = awayTeam.toPresentation(),
-        homeTeamFanRate = homeTeam.toPresentation(),
+        awayTeamFanRate = awayTeam.toUiModel(),
+        homeTeamFanRate = homeTeam.toUiModel(),
+    )
+
+fun TeamFanRateDto.toUiModel(): TeamFanRate =
+    TeamFanRate(
+        team = Team.getByCode(code),
+        teamName = name,
+        fanRate = fanRate,
     )
 
 fun CheckInGameDto.toUiModel(): AttendanceHistoryItem {
@@ -21,8 +36,8 @@ fun CheckInGameDto.toUiModel(): AttendanceHistoryItem {
             id = checkInId,
             attendanceDate = LocalDate.parse(attendanceDate),
             stadiumName = stadiumFullName,
-            awayTeam = awayTeam.toPresentation(homeTeam),
-            homeTeam = homeTeam.toPresentation(awayTeam),
+            awayTeam = awayTeam.toUiModel(homeTeam),
+            homeTeam = homeTeam.toUiModel(awayTeam),
         )
 
     if (homeScoreBoard == null || awayScoreBoard == null || awayTeam.pitcher == null || homeTeam.pitcher == null) {
@@ -33,10 +48,33 @@ fun CheckInGameDto.toUiModel(): AttendanceHistoryItem {
         summary = summary,
         awayTeamPitcher = awayTeam.pitcher,
         homeTeamPitcher = homeTeam.pitcher,
-        awayTeamScoreBoard = awayScoreBoard.toPresentation(),
-        homeTeamScoreBoard = homeScoreBoard.toPresentation(),
+        awayTeamScoreBoard = awayScoreBoard.toUiModel(),
+        homeTeamScoreBoard = homeScoreBoard.toUiModel(),
     )
 }
+
+fun CheckInGameTeamDto.toUiModel(opponent: CheckInGameTeamDto): GameTeam =
+    GameTeam(
+        team = Team.getByCode(code),
+        name = name,
+        score = score?.toString() ?: "-",
+        isMyTeam = isMyTeam,
+        gameResult =
+            if (score == null || opponent.score == null) {
+                GameResult.DRAW
+            } else {
+                GameResult.from(score, opponent.score)
+            },
+    )
+
+fun ScoreBoardDto.toUiModel(): GameScoreBoard =
+    GameScoreBoard(
+        runs = runs,
+        hits = hits,
+        errors = errors,
+        basesOnBalls = basesOnBalls,
+        scores = inningScores,
+    )
 
 fun StadiumCheckInCountDto.toUiModel(): StadiumVisitCount =
     StadiumVisitCount(
