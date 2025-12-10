6ebfc0f315c9aa518e5ac20e7318fb3cd54f75bf
jiyunlee <02leejiyun@naver.com>
2025-12-11 05:11:39 +0900

    feat: BottomNavigation 상단 구분선 추가

--- android/app/src/main/java/com/yagubogu/ui/main/component/MainNavigationBar.kt
+++ android/app/src/main/java/com/yagubogu/ui/main/component/MainNavigationBar.kt
@@ -2,7 +2,9 @@ package com.yagubogu.ui.main.component
 
 import androidx.compose.foundation.interaction.Interaction
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -15,6 +17,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yagubogu.ui.main.BottomNavKey
+import com.yagubogu.ui.theme.Gray200
 import com.yagubogu.ui.theme.Gray500
 import com.yagubogu.ui.theme.PretendardSemiBold12
 import com.yagubogu.ui.theme.Primary500
@@ -29,47 +32,50 @@ fun MainNavigationBar(
     onItemReselect: (BottomNavKey) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    NavigationBar(containerColor = White) {
-        BottomNavKey.items.forEach { item: BottomNavKey ->
-            NavigationBarItem(
-                selected = selectedItem == item,
-                onClick = {
-                    if (selectedItem == item) {
-                        onItemReselect(item)
-                    } else {
-                        onItemClick(item)
-                    }
-                },
-                icon = {
-                    Icon(
-                        painter = painterResource(item.icon),
-                        contentDescription = stringResource(item.label),
-                        modifier = Modifier.size(24.dp),
-                    )
-                },
-                label = {
-                    Text(
-                        text = stringResource(item.label),
-                        style = PretendardSemiBold12,
-                    )
-                },
-                colors =
-                    NavigationBarItemDefaults.colors(
-                        selectedIconColor = Primary500,
-                        selectedTextColor = Primary500,
-                        unselectedIconColor = Gray500,
-                        unselectedTextColor = Gray500,
-                        indicatorColor = White,
-                    ),
-                interactionSource =
-                    object : MutableInteractionSource { // Ripple 효과 제거 위함
-                        override suspend fun emit(interaction: Interaction) {}
-
-                        override fun tryEmit(interaction: Interaction): Boolean = true
-
-                        override val interactions: Flow<Interaction> = emptyFlow()
+    Column {
+        HorizontalDivider(color = Gray200, thickness = 0.4.dp)
+        NavigationBar(containerColor = White) {
+            BottomNavKey.items.forEach { item: BottomNavKey ->
+                NavigationBarItem(
+                    selected = selectedItem == item,
+                    onClick = {
+                        if (selectedItem == item) {
+                            onItemReselect(item)
+                        } else {
+                            onItemClick(item)
+                        }
                     },
-            )
+                    icon = {
+                        Icon(
+                            painter = painterResource(item.icon),
+                            contentDescription = stringResource(item.label),
+                            modifier = Modifier.size(24.dp),
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = stringResource(item.label),
+                            style = PretendardSemiBold12,
+                        )
+                    },
+                    colors =
+                        NavigationBarItemDefaults.colors(
+                            selectedIconColor = Primary500,
+                            selectedTextColor = Primary500,
+                            unselectedIconColor = Gray500,
+                            unselectedTextColor = Gray500,
+                            indicatorColor = White,
+                        ),
+                    interactionSource =
+                        object : MutableInteractionSource { // Ripple 효과 제거 위함
+                            override suspend fun emit(interaction: Interaction) {}
+
+                            override fun tryEmit(interaction: Interaction): Boolean = true
+
+                            override val interactions: Flow<Interaction> = emptyFlow()
+                        },
+                )
+            }
         }
     }
 }
