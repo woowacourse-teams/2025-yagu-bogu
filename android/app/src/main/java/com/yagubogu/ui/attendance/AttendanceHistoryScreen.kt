47c327552529f9b8a9e6075cce818af1e5e34353
jiyunlee <02leejiyun@naver.com>
2025-12-21 20:58:53 +0900

    feat: 직관내역 화면 뷰 타입 토글 스위치 추가

--- android/app/src/main/java/com/yagubogu/ui/attendance/AttendanceHistoryScreen.kt
+++ android/app/src/main/java/com/yagubogu/ui/attendance/AttendanceHistoryScreen.kt
@@ -1,8 +1,14 @@
 package com.yagubogu.ui.attendance
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,14 +16,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,9 +42,14 @@ import com.yagubogu.ui.attendance.component.AttendanceListScreen
 import com.yagubogu.ui.attendance.model.AttendanceHistoryFilter
 import com.yagubogu.ui.attendance.model.AttendanceHistoryItem
 import com.yagubogu.ui.attendance.model.AttendanceHistorySort
+import com.yagubogu.ui.attendance.model.AttendanceHistoryViewType
 import com.yagubogu.ui.theme.Gray050
+import com.yagubogu.ui.theme.Gray200
+import com.yagubogu.ui.theme.Gray500
 import com.yagubogu.ui.theme.PretendardSemiBold20
+import com.yagubogu.ui.theme.White
 import com.yagubogu.ui.util.BackPressHandler
+import com.yagubogu.ui.util.noRippleClickable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -71,6 +88,10 @@ private fun AttendanceHistoryScreen(
     modifier: Modifier = Modifier,
     scrollToTopEvent: SharedFlow<Unit> = MutableSharedFlow(),
 ) {
+    var type: AttendanceHistoryViewType by rememberSaveable {
+        mutableStateOf(AttendanceHistoryViewType.CALENDAR)
+    }
+
     Column(
         modifier =
             modifier
@@ -79,7 +100,11 @@ private fun AttendanceHistoryScreen(
                 .padding(horizontal = 20.dp)
                 .padding(top = 8.dp),
     ) {
-        AttendanceHistoryHeader(currentMonth = YearMonth.now())
+        AttendanceHistoryHeader(
+            currentMonth = YearMonth.now(),
+            type = type,
+            onToggleSwitch = { type = type.toggle() },
+        )
         Spacer(modifier = Modifier.height(12.dp))
         AttendanceListScreen(
             items = items,
@@ -92,27 +117,97 @@ private fun AttendanceHistoryScreen(
 @Composable
 private fun AttendanceHistoryHeader(
     currentMonth: YearMonth,
+    type: AttendanceHistoryViewType,
+    onToggleSwitch: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Row(
         modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
-        Icon(
-            painter = painterResource(R.drawable.ic_arrow_left),
-            contentDescription = null,
-            modifier = Modifier.size(20.dp),
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_arrow_left),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp),
+            )
+            Text(
+                text = "${currentMonth.year}년 ${currentMonth.monthValue}월",
+                style = PretendardSemiBold20,
+            )
+            Icon(
+                painter = painterResource(R.drawable.ic_arrow_right),
+                contentDescription = null,
+                modifier = Modifier.size(20.dp),
+            )
+        }
+        AttendanceViewToggle(
+            type = type,
+            onToggle = onToggleSwitch,
         )
-        Text(
-            text = "${currentMonth.year}년 ${currentMonth.monthValue}월",
-            style = PretendardSemiBold20,
+    }
+}
+
+@Composable
+private fun AttendanceViewToggle(
+    type: AttendanceHistoryViewType,
+    onToggle: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    // -1f: 왼쪽 끝, 1f: 오른쪽 끝
+    val alignBias: Float by animateFloatAsState(
+        targetValue =
+            when (type) {
+                AttendanceHistoryViewType.CALENDAR -> -1f
+                AttendanceHistoryViewType.LIST -> 1f
+            },
+        animationSpec = spring(stiffness = Spring.StiffnessLow),
+    )
+
+    Box(
+        modifier =
+            modifier
+                .height(intrinsicSize = IntrinsicSize.Min)
+                .width(intrinsicSize = IntrinsicSize.Min)
+                .background(color = White, shape = CircleShape)
+                .border(width = 1.dp, color = Gray200, shape = CircleShape)
+                .noRippleClickable(onClick = onToggle)
+                .padding(4.dp),
+    ) {
+        Box(
+            modifier =
+                Modifier
+                    .size(24.dp)
+                    .align(BiasAlignment(horizontalBias = alignBias, verticalBias = 0f))
+                    .background(color = Gray500, shape = CircleShape),
         )
-        Icon(
-            painter = painterResource(R.drawable.ic_arrow_right),
-            contentDescription = null,
-            modifier = Modifier.size(20.dp),
-        )
+
+        Row(
+            modifier =
+                Modifier
+                    .align(Alignment.Center)
+                    .padding(horizontal = 4.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_calendar),
+                contentDescription = null,
+                tint = if (type == AttendanceHistoryViewType.CALENDAR) White else Gray500,
+                modifier = Modifier.size(16.dp),
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Icon(
+                painter = painterResource(R.drawable.ic_list),
+                contentDescription = null,
+                tint = if (type == AttendanceHistoryViewType.LIST) White else Gray500,
+                modifier = Modifier.size(16.dp),
+            )
+        }
     }
 }
 
