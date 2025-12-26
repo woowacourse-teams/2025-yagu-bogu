32a613824c0fd6abd123ad90033105e53db960dd
medAndro <med4ndro@gmail.com>
2025-12-27 02:53:05 +0900

    fix: 현장톡 채팅이 적을떄 하단 정렬되도록 수정

--- android/app/src/main/java/com/yagubogu/ui/livetalk/chat/component/LivetalkChatBubbleList.kt
+++ android/app/src/main/java/com/yagubogu/ui/livetalk/chat/component/LivetalkChatBubbleList.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -65,7 +66,7 @@ fun LivetalkChatBubbleList(
         state = listState,
         reverseLayout = true,
         contentPadding = PaddingValues(horizontal = 20.dp, vertical = 12.dp),
-        verticalArrangement = Arrangement.spacedBy(14.dp),
+        verticalArrangement = Arrangement.spacedBy(14.dp, Alignment.Bottom),
     ) {
         items(
             items = chatItems,
