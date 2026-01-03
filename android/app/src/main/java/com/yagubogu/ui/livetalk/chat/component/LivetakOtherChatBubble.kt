76ff81d7eea8302e2b599619b597fd3431e601d4
medAndro <med4ndro@gmail.com>
2026-01-03 22:12:19 +0900

    refactor: LivetalkOtherChatBubble의 AsyncImage를 ProfileImage 컴포넌트로 교체

--- android/app/src/main/java/com/yagubogu/ui/livetalk/chat/component/LivetakOtherChatBubble.kt
+++ android/app/src/main/java/com/yagubogu/ui/livetalk/chat/component/LivetakOtherChatBubble.kt
@@ -1,7 +1,6 @@
 package com.yagubogu.ui.livetalk.chat.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -21,17 +19,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import com.yagubogu.R
 import com.yagubogu.presentation.util.formatTimestamp
+import com.yagubogu.ui.common.component.profile.ProfileImage
 import com.yagubogu.ui.livetalk.chat.model.LivetalkChatItem
 import com.yagubogu.ui.theme.Black
-import com.yagubogu.ui.theme.Gray300
 import com.yagubogu.ui.theme.Gray400
 import com.yagubogu.ui.theme.Gray500
 import com.yagubogu.ui.theme.Gray900
@@ -68,17 +64,11 @@ fun LivetalkOtherChatBubble(
                     .noRippleClickable(onClick = onProfileClick),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            AsyncImage(
-                model = livetalkChatItem.profileImageUrl,
-                contentDescription = null,
+            ProfileImage(
+                imageUrl = livetalkChatItem.profileImageUrl ?: "",
                 modifier =
                     Modifier
-                        .size(24.dp)
-                        .clip(CircleShape)
-                        .border(1.dp, Gray300, CircleShape),
-                placeholder = painterResource(R.drawable.ic_users),
-                error = painterResource(R.drawable.ic_users),
-                contentScale = ContentScale.Crop,
+                        .size(24.dp),
             )
             Spacer(Modifier.width(8.dp))
             Text(
