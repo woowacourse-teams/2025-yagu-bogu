7d71f034c49a92d24b97ce697f652e01801e62dd
ijh1298 <imjh0417@gmail.com>
2026-01-13 16:34:56 +0900

    docs: NavigationRoot.kt에 KDoc 주석 추가

--- android/app/src/main/java/com/yagubogu/ui/navigation/NavigationRoot.kt
+++ android/app/src/main/java/com/yagubogu/ui/navigation/NavigationRoot.kt
@@ -11,6 +11,14 @@ import androidx.navigation3.ui.NavDisplay
 import com.yagubogu.ui.main.MainScreen
 import com.yagubogu.ui.setting.SettingScreen
 
+/**
+ * 앱의 최상위 네비게이션 구조를 정의하는 루트 컴포저블.
+ *
+ * 각 경로([Route])에 따른 화면 컴포저블을 매핑하여 화면 전환을 관리합니다.
+ * 자식 컴포저블과 분리된 독립적인 [Navigator]를 사용합니다.
+ *
+ * @param modifier 레이아웃 수정을 위한 [Modifier]
+ */
 @Composable
 fun NavigationRoot(modifier: Modifier = Modifier) {
     val navigationState: NavigationState =
