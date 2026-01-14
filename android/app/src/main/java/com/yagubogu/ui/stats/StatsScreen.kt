59d76cc48efd73f40d56f55f786e39c9676f6c5c
jiyunlee <02leejiyun@naver.com>
2026-01-14 12:41:58 +0900

    refactor: 통계 화면 탭 UI 변경

--- android/app/src/main/java/com/yagubogu/ui/stats/StatsScreen.kt
+++ android/app/src/main/java/com/yagubogu/ui/stats/StatsScreen.kt
@@ -1,44 +1,37 @@
 package com.yagubogu.ui.stats
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabPosition
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.yagubogu.ui.stats.detail.StatsDetailScreen
 import com.yagubogu.ui.stats.detail.StatsDetailViewModel
 import com.yagubogu.ui.stats.my.StatsMyScreen
 import com.yagubogu.ui.stats.my.StatsMyViewModel
-import com.yagubogu.ui.theme.PretendardBold
-import com.yagubogu.ui.theme.PretendardSemiBold16
-import com.yagubogu.ui.theme.Primary050
-import com.yagubogu.ui.theme.Primary100
+import com.yagubogu.ui.theme.Gray200
+import com.yagubogu.ui.theme.Gray700
+import com.yagubogu.ui.theme.PretendardSemiBold
 import com.yagubogu.ui.theme.Primary500
-import com.yagubogu.ui.theme.Primary700
 import com.yagubogu.ui.theme.White
 import com.yagubogu.ui.util.BackPressHandler
-import com.yagubogu.ui.util.rememberNoRippleInteractionSource
+import com.yagubogu.ui.util.noRippleClickable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
@@ -73,60 +66,81 @@ fun StatsScreen(
 }
 
 @Composable
+private fun StatsHeader(
+    pagerState: PagerState,
+    coroutineScope: CoroutineScope,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier =
+            modifier
+                .padding(top = 8.dp)
+                .padding(horizontal = 20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        StatsTabRow(
+            pagerState = pagerState,
+            coroutineScope = coroutineScope,
+        )
+    }
+}
+
+@Composable
 private fun StatsTabRow(
     pagerState: PagerState,
     coroutineScope: CoroutineScope,
     modifier: Modifier = Modifier,
 ) {
-    TabRow(
-        selectedTabIndex = pagerState.currentPage,
-        containerColor = Primary050,
-        contentColor = Primary500,
-        indicator = { tabPositions: List<TabPosition> ->
-            TabRowDefaults.PrimaryIndicator(
-                modifier =
-                    Modifier
-                        .tabIndicatorOffset(tabPositions[pagerState.currentPage])
-                        .zIndex(-1f), // 텍스트 뒤로 Indicator 배경을 그리기 위함
-                width = tabPositions[pagerState.currentPage].width - 12.dp,
-                height = 48.dp,
-                color = Primary500,
-                shape = RoundedCornerShape(12.dp),
-            )
-        },
-        divider = {},
-        modifier =
-            modifier
-                .padding(top = 8.dp, start = 20.dp, end = 20.dp)
-                .height(60.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .border(1.dp, Primary100, RoundedCornerShape(12.dp)),
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
     ) {
         StatsTab.entries.forEachIndexed { index: Int, tab: StatsTab ->
-            val isSelected = pagerState.currentPage == index
+            val isSelected: Boolean = pagerState.currentPage == index
 
-            Tab(
-                selected = isSelected,
-                onClick = {
-                    coroutineScope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
-                },
-                interactionSource = rememberNoRippleInteractionSource(),
-                content = {
-                    val style: TextStyle =
-                        if (isSelected) {
-                            PretendardBold.copy(color = White, fontSize = 18.sp)
-                        } else {
-                            PretendardSemiBold16.copy(color = Primary700)
+            StatsTab(
+                title = stringResource(tab.titleResId),
+                isSelected = isSelected,
+                modifier =
+                    Modifier.noRippleClickable {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
                         }
-                    Text(
-                        text = stringResource(tab.titleResId),
-                        style = style,
-                        modifier = Modifier.padding(12.dp),
-                    )
-                },
+                    },
             )
         }
     }
+}
+
+@Composable
+private fun StatsTab(
+    title: String,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = title,
+        style =
+            PretendardSemiBold.copy(
+                fontSize = 18.sp,
+                color = if (isSelected) White else Gray700,
+            ),
+        modifier =
+            modifier
+                .background(color = if (isSelected) Primary500 else White, shape = CircleShape)
+                .border(
+                    width = 1.dp,
+                    color = if (isSelected) Primary500 else Gray200,
+                    shape = CircleShape,
+                ).padding(horizontal = 20.dp, vertical = 12.dp),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun StatsHeaderPreview() {
+    StatsHeader(
+        pagerState = rememberPagerState(pageCount = { StatsTab.entries.size }),
+        coroutineScope = rememberCoroutineScope(),
+    )
 }
