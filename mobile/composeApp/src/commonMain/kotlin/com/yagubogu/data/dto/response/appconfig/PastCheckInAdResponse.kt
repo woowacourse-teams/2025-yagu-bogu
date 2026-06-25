25eed6314e6c1ae81770a619b6c308c96d8fbdb4
ijh1298 <imjh0417@gmail.com>
2026-06-25 23:00:53 +0900

    refactor: PastCheckInAdResponse 필드 설명 주석 추가

--- mobile/composeApp/src/commonMain/kotlin/com/yagubogu/data/dto/response/appconfig/PastCheckInAdResponse.kt
+++ mobile/composeApp/src/commonMain/kotlin/com/yagubogu/data/dto/response/appconfig/PastCheckInAdResponse.kt
@@ -6,9 +6,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class PastCheckInAdResponse(
     @SerialName("is_enabled")
-    val isEnabled: Boolean = true,
+    val isEnabled: Boolean = true, // 광고 활성화 여부
     @SerialName("start_count")
-    val startCount: Int = 2,
+    val startCount: Int = 2, // 광고가 처음 노출되는 과거 직관 추가 횟수
     @SerialName("interval")
-    val interval: Int = 3,
+    val interval: Int = 3, // 이후 반복 노출 간격
 )
