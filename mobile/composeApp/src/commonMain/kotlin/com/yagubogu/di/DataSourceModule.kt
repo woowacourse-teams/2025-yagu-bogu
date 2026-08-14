5dd7baa5333c371fa8c7489a8c8b3122e8facbeb
ijh1298 <imjh0417@gmail.com>
2026-08-14 11:03:58 +0900

    chore: 플레이스 목 데이터 소스 제거, 실 API로 전환
    백엔드 /api/v1/places 배포 완료. PlaceMockDataSource와
    USE_PLACE_MOCK_DATA 플래그 제거하고 PlaceRemoteDataSource를
    기본 바인딩으로 되돌림.

--- mobile/composeApp/src/commonMain/kotlin/com/yagubogu/di/DataSourceModule.kt
+++ mobile/composeApp/src/commonMain/kotlin/com/yagubogu/di/DataSourceModule.kt
@@ -13,7 +13,6 @@ import com.yagubogu.data.datasource.game.GameRemoteDataSource
 import com.yagubogu.data.datasource.member.MemberDataSource
 import com.yagubogu.data.datasource.member.MemberRemoteDataSource
 import com.yagubogu.data.datasource.place.PlaceDataSource
-import com.yagubogu.data.datasource.place.PlaceMockDataSource
 import com.yagubogu.data.datasource.place.PlaceRemoteDataSource
 import com.yagubogu.data.datasource.stadium.StadiumDataSource
 import com.yagubogu.data.datasource.stadium.StadiumRemoteDataSource
@@ -32,21 +31,13 @@ import org.koin.dsl.module
 
 expect fun Module.registerPlatformDataSources()
 
-// 백엔드 /api/v1/places API가 아직 배포되지 않아 임시로 목 데이터를 쓴다.
-// 실제 API 배포 후 false로 되돌린다.
-private const val USE_PLACE_MOCK_DATA = true
-
 val datasourceModule =
     module {
         singleOf(::AuthRemoteDataSource) { bind<AuthDataSource>() }
 
         singleOf(::MemberRemoteDataSource) { bind<MemberDataSource>() }
 
-        if (USE_PLACE_MOCK_DATA) {
-            singleOf(::PlaceMockDataSource) { bind<PlaceDataSource>() }
-        } else {
-            singleOf(::PlaceRemoteDataSource) { bind<PlaceDataSource>() }
-        }
+        singleOf(::PlaceRemoteDataSource) { bind<PlaceDataSource>() }
 
         singleOf(::CheckInRemoteDataSource) { bind<CheckInDataSource>() }
 
